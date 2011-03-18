16d6c5228ab9b14193df8007e779bd4ee5e6948f
Matthieu Bontemps <matthieu@knplabs.com>
2011-03-18 22:09:19 +0100

    Render labels instead of names

--- Resources/views/Menu/item.html.php
+++ Resources/views/Menu/item.html.php
@@ -1,6 +1,6 @@
 <?php if($item->shouldBeRendered()): ?>
     <li <?php echo $view['menu']->attributes($view['menu']->getItemAttributes($item)) ?>>
-        <a href="<?php echo $item->getUri() ?>"><?php echo $item->getName() ?></a>
+        <a href="<?php echo $item->getUri() ?>"><?php echo $item->getLabel() ?></a>
     </li>
     <?php if($item->hasChildren()): ?>
         <?php echo $view->render('KnplabsMenuBundle:Menu:menu.html.php', array('item' => $item)) ?>
